1d240dccb17a0a726095cc91323eec6d19bac0f2
Grzegorz121979 <pociechagrzegorz@outlook.com>
2023-05-19 20:24:33 +0200

    function to calculate salary

--- nowy 1.py
+++ nowy 1.py
@@ -1,4 +1,10 @@
+def calculate_salary(h, r):
+    return round((h* r), 2)
+
+
 hour = float(input('Enter hour: '))
 rate = float(input('Enter rate: '))
-pay = hour * rate
-print(f'Your salary is ${round(pay, 2)}')
+
+pay = calculate_salary(hour, rate)
+
+print(f'Your salary is ${pay}')
